e90e21f6a04c4d40df733034bc73b517264cac37
nickriordan <nick@anamatica.com>
2025-12-04 21:15:25 +0000

    Day 2

--- src/main/kotlin/com/anamatica/aoc2025/day01.kt
+++ src/main/kotlin/com/anamatica/aoc2025/day01.kt
@@ -1,17 +1,13 @@
 package com.anamatica.aoc2025
 
 fun main() {
-    println(part1())
-    println(part2())
-}
+    fun rotateLeft(startPos: Int, count: Int) =
+        generateSequence((startPos + 99) % 100) { (it + 99) % 100 }.take(count).toList()
 
-fun rotateLeft(startPos: Int, count: Int) =
-    generateSequence((startPos + 99) % 100) { (it + 99) % 100 }.take(count).toList()
+    fun rotateRight(startPos: Int, count: Int) =
+        generateSequence((startPos + 1) % 100) { (it + 1) % 100 }.take(count).toList()
 
-fun rotateRight(startPos: Int, count: Int) =
-    generateSequence((startPos + 1) % 100) { (it + 1) % 100 }.take(count).toList()
-
-fun part1() = loadFileAsLines("/day01.txt").fold((50 to 0)) { (position, countZero), line ->
+    fun part1() = loadFileAsLines("/day01.txt").fold((50 to 0)) { (position, countZero), line ->
         val rotateBy = line.drop(1).toInt() % 100
 
         val newPos = if (line.first() == 'L') {
@@ -22,14 +18,19 @@ fun part1() = loadFileAsLines("/day01.txt").fold((50 to 0)) { (position, countZe
         (newPos to if (newPos == 0) countZero + 1 else countZero)
     }.second
 
-fun part2() = loadFileAsLines("/day01.txt").fold((50 to 0)) { (position, countZero), line ->
-    val rotateBy = line.drop(1).toInt()
+    fun part2() = loadFileAsLines("/day01.txt").fold((50 to 0)) { (position, countZero), line ->
+        val rotateBy = line.drop(1).toInt()
 
-    val list = if (line.first() == 'L') {
-        rotateLeft(position, rotateBy)
-    } else {
-        rotateRight(position, rotateBy)
-    }
+        val list = if (line.first() == 'L') {
+            rotateLeft(position, rotateBy)
+        } else {
+            rotateRight(position, rotateBy)
+        }
 
-    list.last() to countZero + (list.count { it == 0 })
-}.second
+        list.last() to countZero + (list.count { it == 0 })
+    }.second
+
+    println(part1())
+    println(part2())
+}
+
